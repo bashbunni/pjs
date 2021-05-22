69238ea6658bc09d48767843156e98afc27f7a87
bashbunni <bashbunni.io@gmail.com>
2021-05-21 18:28:09 -0700

    adding reliably

--- project.go
+++ project.go
@@ -9,7 +9,7 @@ import (
 
 type Project struct {
 	gorm.Model
-	Id      string
+	Id      uint32
 	Message string
 }
 
@@ -17,28 +17,37 @@ func (p Project) getMsg() string {
 	return p.Message
 }
 
-func (p Project) getId() string {
+func (p Project) getId() uint32 {
 	return p.Id
 }
 
 func (p Project) latest() {
-	fmt.Printf("%s : %s\n", p.Id, p.Message)
+	fmt.Printf("%d : %s\n", p.getId(), p.getMsg())
+}
+
+func (p *Project) add(message string, db *gorm.DB) {
+	db.Create(&Project{Message: message})
 }
 
 func main() {
+	// setup
 	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
+	// migrate the schema
 	db.AutoMigrate(&Project{})
-	//	db.Create(&Project{Id: "12345", Message: "hello"})
 
+	// other things
 	var project Project
+
+	project.add("bonk", db)
+
 	var projects []Project
 	db.Find(&projects)
 	db.First(&project)
-	project.latest()
+	//	project.latest()
 
 	for _, proj := range projects {
 		proj.latest()
