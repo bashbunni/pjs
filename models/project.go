4924d2626b9cb15bda6e74c0861a700233b446be
bashbunni <bashbunni.io@gmail.com>
2021-09-13 19:24:36 -0700

    feat: implement MockProjectRepository for tests

--- models/project.go
+++ models/project.go
@@ -6,16 +6,53 @@ import (
 	"gorm.io/gorm"
 )
 
+/*
+have a struct for each source of data: e.g. DB, []Project
+*/
+
 const notFound uint = 0
 
+// Entity
 type Project struct {
 	gorm.Model
 	Name string
 }
 
-func PrintProjects(db *gorm.DB) {
-	if hasProjects(db) {
-		projects := GetAllProjects(db)
+// Interface
+type ProjectRepository interface {
+	GetOrCreateProjectByID(projectID int) Project
+	PrintProjects()
+	hasProjects() bool
+	getProjectByID(projectId int, db *gorm.DB) Project
+	GetAllProjects() []Project
+	CreateProject(name string) Project
+}
+
+// Mock Implementation
+type MockProjectRepository struct {
+	Projects []Project
+}
+
+func (m MockProjectRepository) GetOrCreateProjectByID(projectID uint) Project {
+	proj := m.getProjectByID(projectID)
+	if proj.ID == notFound {
+		return m.CreateProject("")
+	}
+	return proj
+}
+
+func (m MockProjectRepository) getProjectByID(projectID uint) Project {
+	for _, p := range m.Projects {
+		if projectID == p.ID {
+			return p
+		}
+	}
+	return Project{}
+}
+
+func (m MockProjectRepository) PrintProjects() {
+	if m.hasProjects() {
+		projects := m.GetAllProjects()
 		for _, project := range projects {
 			fmt.Printf(Format, project.ID, project.Name)
 		}
