1ed4c7d90ecc18f3160c3855ae78278bc499f6c8
bashbunni <bashbunni.io@gmail.com>
2022-01-09 12:35:05 -0800

    feat: add outline of test files

---
+++ tests/project_test.go
@@ -0,0 +1,22 @@
+package main
+
+import "testing"
+
+func TestHelloWorld(t *testing.T) {
+	// t.Fatal("not implemented")
+}
+
+/*
+TODO:
+Things to test:
+type ProjectRepository interface {
+	GetOrCreateProjectByID(projectID int) Project
+	PrintProjects()
+	hasProjects() bool
+	getProjectByID(projectId int) Project
+	GetAllProjects() ([]Project, error)
+	CreateProject(name string) Project
+	DeleteProject(pe *ProjectWithEntries, er EntryRepository)
+	RenameProject(pe *ProjectWithEntries)
+}
+*/
