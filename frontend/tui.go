0e78c012438fda31ea4aa51c602d299a6e2843e7
bashbunni <bashbunni.io@gmail.com>
2022-02-17 13:38:04 -0800

    feat: add glamour view

--- frontend/tui.go
+++ frontend/tui.go
@@ -18,6 +18,35 @@ import (
 // TODO: render list of entries -> need to update m.list with entries instead of projects and change title to proj name
 // TODO: stop cursor from moving in edit mode -> override list.Model
 
+const content = `
+Today’s Menu
+| Name        | Price | Notes                           |
+| ---         | ---   | ---                             |
+| Tsukemono   | $2    | Just an appetizer               |
+| Tomato Soup | $4    | Made with San Marzano tomatoes  |
+| Okonomiyaki | $4    | Takes a few minutes to make     |
+| Curry       | $3    | We can add squash if you’d like |
+Seasonal Dishes
+| Name                 | Price | Notes              |
+| ---                  | ---   | ---                |
+| Steamed bitter melon | $2    | Not so bitter      |
+| Takoyaki             | $3    | Fun to eat         |
+| Winter squash        | $3    | Today it's pumpkin |
+Desserts
+| Name         | Price | Notes                 |
+| ---          | ---   | ---                   |
+| Dorayaki     | $4    | Looks good on rabbits |
+| Banana Split | $5    | A classic             |
+| Cream Puff   | $3    | Pretty creamy!        |
+All our dishes are made in-house by Karen, our chef. Most of our ingredients
+are from our garden or the fish market down the street.
+Some famous people that have eaten here lately:
+* [x] René Redzepi
+* [x] David Chang
+* [ ] Jiro Ono (maybe some day)
+Bon appétit!
+`
+
 var docStyle = lipgloss.NewStyle().Margin(1, 2)
 
 type item struct {
@@ -53,9 +82,6 @@ func (m model) Init() tea.Cmd {
 	return nil
 }
 
-// organize Update, View by state
-// are we checking the messages for the state?
-// I'm looking to do different things with each message depending on the state
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmds []tea.Cmd
 	var cmd tea.Cmd
@@ -70,15 +96,52 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 }
 
 func (m model) View() string {
-	if m.input.Focused() {
-		return docStyle.Render(m.list.View() + "\n" + m.input.View())
+	switch m.state {
+	case "viewEntries":
+		return docStyle.Render(m.viewport.View())
+	default:
+		if m.input.Focused() {
+			return docStyle.Render(m.list.View() + "\n" + m.input.View())
+		}
+		return docStyle.Render(m.list.View() + "\n")
 	}
-	return docStyle.Render(m.list.View() + "\n")
+}
+func (m model) handleEntriesList(msg tea.Msg, cmds []tea.Cmd, cmd tea.Cmd) (model, tea.Cmd) {
+	switch msg := msg.(type) {
+	case tea.WindowSizeMsg:
+		m.viewport.Width = msg.Width
+		// check for my cmds being done running
+		// check for keypresses?
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		default:
+			m.viewport, cmd = m.viewport.Update(msg)
+			cmds = append(cmds, cmd)
+		}
+	}
+	return m, tea.Batch(cmds...)
 }
 
-// functions
+func initEntries(m tea.Model) (viewport.Model, error) {
+	vp := viewport.New(78, 20)
+	vp.Style = lipgloss.NewStyle().
+		BorderStyle(lipgloss.RoundedBorder()).
+		BorderForeground(lipgloss.Color("62")).
+		PaddingRight(2)
+	renderer, err := glamour.NewTermRenderer(glamour.WithStylePath("notty"))
+	if err != nil {
+		return vp, err
+	}
+	str, err := renderer.Render(content)
+	if err != nil {
+		return vp, err
+	}
+	vp.SetContent(str)
+	return vp, nil
+}
 
-// state functions
 func StartTea(pr models.GormProjectRepository, er models.GormEntryRepository) {
 	input := textinput.New()
 	input.Prompt = "$ "
@@ -92,57 +155,11 @@ func StartTea(pr models.GormProjectRepository, er models.GormEntryRepository) {
 	}
 	items := projectsToItems(projects)
 	m := initProjectView(items, input, &pr, &er)
-
 	tea.LogToFile("debug.log", "debug")
 	p := tea.NewProgram(m)
 	p.EnterAltScreen()
-
 	if err := p.Start(); err != nil {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
 	}
-}
-
-func (m model) handleEntriesList(msg tea.Msg, cmds []tea.Cmd, cmd tea.Cmd) (model, tea.Cmd) {
-	m.initEntries()
-	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.viewport.Width = msg.Width
-		// check for my cmds being done running
-		// check for keypresses?
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "ctrl+c":
-			return m, tea.Quit
-		default:
-			m.viewport, cmd = m.viewport.Update(msg)
-		}
-	}
-	return m, tea.Batch(cmds...)
-}
-
-func (m model) initEntries() error {
-	content := `# Hi
-	I'm back
-	## titles
-	- and stuff`
-	// change state to viewEntries
-	m.state = "viewEntries"
-	// init viewport (per glamour example)
-	vp := viewport.New(78, 20)
-	vp.Style = lipgloss.NewStyle().
-		BorderStyle(lipgloss.RoundedBorder()).
-		BorderForeground(lipgloss.Color("62")).
-		PaddingRight(2)
-	renderer, err := glamour.NewTermRenderer(glamour.WithStylePath("notty"))
-	if err != nil {
-		return err
-	}
-	str, err := renderer.Render(content)
-	if err != nil {
-		return err
-	}
-	vp.SetContent(str)
-	m.viewport = vp
-	return nil
 }
