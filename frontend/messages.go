4e042d07d8dff04589f90e098e30a8c1dcb0535f
bashbunni <bashbunni.io@gmail.com>
2022-02-19 23:12:35 -0800

    refactor: clean up code smell

--- frontend/messages.go
+++ frontend/messages.go
@@ -1,16 +1,6 @@
 package frontend
 
-import "github.com/bashbunni/project-management/models"
-
-type updateEntryListMsg struct {
-	entries []models.Entry
-}
-type errMsg struct{ error }
-
-// TODO: have this implement Error()
-
-// project
-
+type errMsg struct{ error } // TODO: have this implement Error()
 type updateProjectListMsg struct{}
-
 type renameProjectMsg struct{}
+type updateEntryListMsg struct{}
